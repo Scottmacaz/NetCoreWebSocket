cd9688f83fefce2c1a16b955a2bd6453abc1a68c
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2020-02-27 14:58:43 -0800

    Added records controller

---
+++ Server/Server/Services/RecordService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Services
+{
+    public class RecordService
+    {
+        public void GetRecords()
+        {
+            int totalRecords = 20000;
+            int currentRecord = 0;
+
+            while (currentRecord < totalRecords)
+            {
+
+            }
+        }
+    }
+}
